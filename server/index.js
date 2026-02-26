a93291469a609e85e322fdc1a78a121e5ef84f61
kartikey <kartikeysingh99999@gmail.com>
2026-02-26 22:03:06 +0530

    minor modification

--- server/index.js
+++ server/index.js
@@ -5,7 +5,15 @@ const fs = require("fs");
 const vision = require("@google-cloud/vision");
 const pdfParse = require("pdf-parse");
 const db = require("./firebase");
+import admin from "firebase-admin";
 
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  }),
+});
 const app = express();
 
 app.use(cors());
